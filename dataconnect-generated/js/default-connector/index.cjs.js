47f8389e86a1188144488b49f57be7697ca9f1f5
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-02-12 22:24:11 +0800

    INTEGRATION
    Firebase Integration

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'wildfindwebapp',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
