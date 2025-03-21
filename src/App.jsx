373b894d60421d8ee018626380c4fdb93e158291
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-03-21 22:45:10 +0800

    update for myself
    admin-header_sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Admin from "./features/Dashboard/Admin-Dashboard/admin-dashboard";
 import UserDashboard from "./features/Dashboard/User_Dashboard/dashboard";
 import LandingPage from "./features/Landing_Page/LandingPage";
 import ContactUs from "./features/Home/contact-us"; // Import ContactUs
+import AdminLayout from "./components/Admin/AdminLayout"; 
 import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/adminLayout" element={<AdminLayout />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<Admin />} />
         <Route path="/login" element={<AdminLogin />} />
