4576dc09c11ce3b4f77f19c9f0f32fdc98869663
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-03-22 18:19:40 +0800

    push now to main origin
    push now to main origin

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import UserDashboard from "./features/Dashboard/User_Dashboard/dashboard";
 import LandingPage from "./features/Landing_Page/LandingPage";
 import ContactUs from "./features/Home/contact-us"; // Import ContactUs
 import URF from "./features/UserRequestForm/RequestForm";
+import AdminLayout from "./components/Admin/AdminLayout";
 import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/*<Route path="/adminLayout" element={<AdminLayout />} /> */}
+        <Route path="/adminLayout" element={<AdminLayout />} /> 
         <Route path="/urf" element={<URF />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<Admin />} />
