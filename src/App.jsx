2eea47364c3d893a874ed4bad61698ced6922bd5
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-03-22 18:46:21 +0800

    push to my branch only
    push to my branch only

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Admin from "./features/Dashboard/Admin-Dashboard/admin-dashboard";
 import UserDashboard from "./features/Dashboard/User_Dashboard/dashboard";
 import LandingPage from "./features/Landing_Page/LandingPage";
 import ContactUs from "./features/Home/contact-us"; // Import ContactUs
-import RFI from "./features/ReportFoundItem/ReportFoundItem";
+import URF from "./features/UserRequestForm/RequestForm";
+import AdminLayout from "./components/Admin/AdminLayout";
 import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/landingPage" element={<LandingPage />} /> 
+        <Route path="/adminLayout" element={<AdminLayout />} /> 
+        <Route path="/urf" element={<URF />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<Admin />} />
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/contact-us" element={<ContactUs />} /> {/* Added route for Contact Us */}
-        <Route path="/" element={<RFI />} /> {/* LandingPage */}
+        <Route path="/" element={<LandingPage />} /> {/* Added route for Contact Us */}
       </Routes>
     </Router>
   );
