ccf2c972150efcede5ffad45f5524074db288da9
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-03-23 15:34:40 +0800

    Components Admin Dashboar
    Components Admin Dashboar push by Leader/PM

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,22 @@ import LandingPage from "./features/Landing_Page/LandingPage";
 import ContactUs from "./features/Home/contact-us"; // Import ContactUs
 import URF from "./features/UserRequestForm/RequestForm";
 import AdminLayout from "./components/Admin/AdminLayout";
+import AdminDashboard from "./components/Admin/AdminDashoard/AdminDashboard";
+import StatusTrends from "./components/Admin/StatusTrends/StatusTrends";
+import ClaimedItemsChart from "./components/Admin/ClaimedItemsChart/ClaimedItemsCategory";
+import AdminLog from "./components/Admin/AdminLog/AdminLog";
 import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/landingPage" element={<LandingPage />} /> 
+        <Route path="/AdminLog" element={<AdminLog />} />
+        <Route path="/ClaimedItemsChart" element={<ClaimedItemsChart />} />
+        <Route path="/StatusTrends" element={<StatusTrends />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} /> 
         <Route path="/adminLayout" element={<AdminLayout />} /> 
+        <Route path="/landingPage" element={<LandingPage />} />
         <Route path="/urf" element={<URF />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<Admin />} />
