702dacc2e084091ec984c2bcda2b753cc4cc9f44
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-03-25 02:35:42 +0800

    pushing to my branch
    pushing to my branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ import UserDashboard from "./features/Dashboard/User_Dashboard/dashboard";
 import LandingPage from "./features/Landing_Page/LandingPage";
 import ContactUs from "./features/Contact-us/contact-us"; // Import ContactUs
 import URF from "./features/UserRequestForm/RequestForm";
-import AdminLayout from "./components/Admin/Sdebr-Hdr-Admin/AdminLayout";
 import AdminDashboard from "./components/Admin/AdminDashoard/AdminDashboard";
+import Sdrhdr from "./features/Admin-sdr&hdr/sdr&hdr"; 
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/AdminDashboard" element={<AdminDashboard />} /> 
-        <Route path="/adminLayout" element={<AdminLayout />} /> 
+      <Routes> 
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
+        <Route path="/Sdrhdr" element={<Sdrhdr />} />     
         <Route path="/landingPage" element={<LandingPage />} />
         <Route path="/urf" element={<URF />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<Admin />} />
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/contact-us" element={<ContactUs />} /> {/* Added route for Contact Us */}
-        <Route path="/" element={<LandingPage />} /> {/* Added route for Contact Us */}
+        <Route path="/" element={<LandingPage/>} /> {/* Added route for Contact Us */}
       </Routes>
     </Router>
   );
